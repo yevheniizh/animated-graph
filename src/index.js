f5ad15fcb5eb55740a2ced7f636d3dc9133232db
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-09-14 17:11:08 +0300

    feat: Add fast-simplex-noise animated graph

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./styles.css";
 import gsap from "gsap";
+import FastSimplexNoise from "../node_modules/fast-simplex-noise";
+
+//=============gsap=============//
 
 const width = window.innerWidth;
 const height = 500;
@@ -17,7 +20,7 @@ const state3 = [100, 50, 200, 0, 50, 110, 300];
 
 const uberState = [...state1]; // we need to set percisely a copy of arr
 
-function drawLine(uberState) {
+function drawLine(uberState, ctx) {
   ctx.clearRect(0, 0, width, height); // clear screen
   ctx.beginPath();
   ctx.moveTo(0, uberState[0]);
@@ -32,7 +35,7 @@ function drawLine(uberState) {
 
 const t1 = gsap.timeline({
   onUpdate() {
-    drawLine(uberState);
+    drawLine(uberState, ctx);
   },
   repeat: -1, // create endless loop
 });
@@ -40,3 +43,33 @@ const t1 = gsap.timeline({
 t1.to(uberState, 2, { endArray: state2 }) // second item - animating time, [sec]
   .to(uberState, 2, { endArray: state3 })
   .to(uberState, 2, { endArray: state1 });
+
+//=============fast-simplex-noise=============//
+
+let canvas1 = document.createElement("canvas");
+let ctx1 = canvas1.getContext("2d");
+canvas1.width = width;
+canvas1.height = height;
+document.body.appendChild(canvas1);
+var newstate = [0, 110, 150, 310, 350, 300, 0];
+
+var t = 0;
+const noiseGen = new FastSimplexNoise({
+  frequency: 0.5,
+  max: 400,
+  min: 0,
+  octaves: 1,
+});
+
+function render() {
+  t++;
+  ctx1.lineJoin = "round"; // round line edges
+  newstate = [];
+  for (var i = 0; i < 7; i++) {
+    newstate.push(noiseGen.scaled([i * 5, t / 100]));
+  }
+  drawLine(newstate, ctx1);
+  window.requestAnimationFrame(render);
+}
+
+render();
